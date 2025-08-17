5c6cae42f983907dfb1ed47a8539db2dabe35fba
Juan Figari <jdfigari@gmail.com>
2025-08-17 09:08:15 -0300

    Dos componentes más (Compartido|CopyCat) para profundizar en props y estado propio.

--- src/App.jsx
+++ src/App.jsx
@@ -13,25 +13,42 @@ function App() {
       </div>
       <h1>Un juego incremental</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <UnBoton />
+        <BotonCompartido count={count} setCount={setCount} />
+        <BotonCopyCat valorPadre={count} />
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="change-log">
-        Componente UnBoton
+        Componente UnBoton {count}
       </p>
     </>
   )
 }
 
-function UnBoton() {
+function UnBoton  () {
   const [count, setCount] = useState(0)
+  return <button onClick={() => setCount(count + 5)}>Boton: {count}</button>
+}
 
-  return <button onClick={() => setCount(count + 5)}>Hazme Click {count}</button>
+function BotonCompartido({ count, setCount }) {
+  return <button onClick={() => setCount(count + 1)}>Compartido: {count}</button>
+}
+
+function BotonCopyCat({ valorPadre }) {
+  const [valor, setValor] = useState(valorPadre)
+
+  return (
+    <>
+      <button onClick={() => setValor(valorPadre)}>
+        CopyCat: {valor}
+      </button>
+      <button onClick={() => setValor(valor + 1)}>
+        AugCat: {valor}
+      </button>
+    </>
+  )
 }
 
 export default App
