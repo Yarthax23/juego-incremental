bf8a7c348e4b09e7335fe44b5018c8643606c5ad
Juan Figari <jdfigari@gmail.com>
2025-08-17 08:33:38 -0300

    Cambié title, h1 y armé componente independiente para Botón.

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,29 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        
       </div>
-      <h1>Vite + React</h1>
+      <h1>Un juego incremental</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <UnBoton />
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p className="change-log">
+        Componente UnBoton
       </p>
     </>
   )
+}
+
+function UnBoton() {
+  const [count, setCount] = useState(0)
+
+  return <button onClick={() => setCount(count + 5)}>Hazme Click {count}</button>
 }
 
 export default App
